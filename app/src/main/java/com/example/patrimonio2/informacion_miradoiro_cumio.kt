3139c8301720f101a93ba4742cc152c4a854cab1
sonianunez <soninhalestedo9389@gmail.com>
2022-05-11 09:21:45 +0200

    codigos cambiados  añadir imagenes
     imagenes nuevas

--- app/src/main/java/com/example/patrimonio2/informacion_miradoiro_cumio.kt
+++ app/src/main/java/com/example/patrimonio2/informacion_miradoiro_cumio.kt
@@ -29,7 +29,7 @@ class informacion_miradoiro_cumio : AppCompatActivity() {
 
         val maps = findViewById<ImageButton>(R.id.maps)
         maps.setOnClickListener {
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/maps/place/42%C2%B048'54.5%22N+8%C2%B037'19.1%22W/@42.8151251,-8.624168,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0xb763a8e60fa1d8f6!8m2!3d42.8151251!4d-8.6219793")))
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/maps/place/42%C2%B050'03.3%22N+8%C2%B032'29.4%22W/@42.8342554,-8.5436829,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x13de51623810adb7!8m2!3d42.8342554!4d-8.5414942")))
         }
 
         val informacion = findViewById<ImageView>(R.id.informacion)
